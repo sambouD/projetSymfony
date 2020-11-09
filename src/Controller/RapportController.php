a0c1cb071df9bf073de33d2a1db914cdecaf9242
Vexco <xmantellato@gmail.com>
2020-11-09 17:06:50 +0100

    Version0.01 Xavier

---
+++ src/Controller/RapportController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RapportController extends AbstractController
+{
+    /**
+     * @Route("/rapport", name="rapport_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('rapport/index.html.twig', [
+            'controller_name' => 'RapportController',
+        ]);
+    }
+}
