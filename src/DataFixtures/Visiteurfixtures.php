4a90d7029d8bd8404647be802bda615f7534eb5d
sambouD <hungsas93@gmail.com>
2020-11-10 18:17:54 +0100

    vers3

--- src/DataFixtures/Visiteurfixtures.php
+++ src/DataFixtures/Visiteurfixtures.php
@@ -3,9 +3,12 @@
 namespace App\DataFixtures;
 
 use App\Entity\ActiviteComplet;
+use App\Entity\Inviter;
 use App\Entity\Medicament;
+use App\Entity\Offrir;
 use Faker\Factory;
 use App\Entity\Praticien;
+use App\Entity\RapportVisite;
 use App\Entity\Visiteur;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -17,7 +20,9 @@ class Visiteurfixtures extends Fixture
         $faker = Factory::create('FR-fr');
 
         //ActiviteComplet
-        for ($i=1; $i <=20 ; $i++) { 
+        $activiteComplets = [];// Foreign Key
+
+        for ($i=0; $i <=30 ; $i++) { 
            
             $activiteComplet = new ActiviteComplet();
 
@@ -30,12 +35,14 @@ class Visiteurfixtures extends Fixture
                             ->setAcTheme($acTheme);
         
             $manager->persist($activiteComplet);
+            $activiteComplets[] = $activiteComplet; // Foreign Key
         }
 
         //Praticien
         $genres = ["male" , "female"];
+        $praticiens =[];// Foreign Key
 
-        for ($i=1; $i <=30 ; $i++) { 
+        for ($i=0; $i <= 30 ; $i++) { 
             # code...
             $praticien = new Praticien();
             
@@ -44,13 +51,15 @@ class Visiteurfixtures extends Fixture
                       ->setPrenom($faker->lastName($genres[mt_rand(0,1)]));
             
             $manager->persist($praticien);
+            $praticiens[] = $praticien; // Foreign Key
         }
 
         //Visiteur
+        
+        $visiteurs=[];// Foreign Key
 
-        for ($i=0; $i <= 40; $i++) { 
+        for ($i=0; $i <= 30; $i++) { 
             $visiteur = new Visiteur();
-
        
             $login = $faker->userName();
             $mdp = $faker->password();
@@ -69,9 +78,11 @@ class Visiteurfixtures extends Fixture
                      ->setDateEmbouche($dateEmbauche);
         
             $manager->persist($visiteur);  
+            $visiteurs[] = $visiteur; // Foreign Key
         }
 
     //Medicament
+        $med = []; // Foreign Key
 
         $medicaments=fopen(__DIR__."/medicaments.csv","r");
         while (!feof($medicaments)) {
@@ -85,10 +96,62 @@ class Visiteurfixtures extends Fixture
                 ->setLibelle($value[1]);
         
         $manager->persist($medicament); 
+        
+        $med[] = $medicament; // Foreign Key
     }
 
     //rapport_visite
+    $rapport_visites = []; // Foreign Key
+
+    for ($i=0; $i <=30; $i++) { 
+        $rapport_visite = new RapportVisite();
+
+        $rap_date = $faker->dateTimeBetween('-10 years' , 'now');  
+        $rap_motif= $faker->text(10);
+        $rap_bilan = $faker->sentence(3, true);
+
+        $rapport_visite->setVisiteur($visiteurs[mt_rand(0,30)])
+                        ->setPraticien($praticiens[mt_rand(0,30)])
+                        ->setRapDate($rap_date)
+                        ->setRapBilan($rap_bilan)
+                        ->setRapMotif($rap_motif);
+
+        $manager->persist($rapport_visite); 
+        $rapport_visites[] = $rapport_visite; // Foreign Key
+    }
+
+    //Inviter
+    for ($i=0; $i <= 30 ; $i++) { 
+        $inviter = new Inviter();
+        
+        $spe = $faker->word();
+
+        $inviter->setPraticien($praticiens[mt_rand(0,30)])
+                ->setActiviteCompl($activiteComplets[mt_rand(0,30)])
+                ->setSpecialisation($spe);
+
+        $manager->persist($inviter);
+    }
+
+    //Offrir 
+
+    for ($i=0; $i <= 20; $i++) { 
+        
+        $offrir = new Offrir();
+
+        //Faker 
+        $qte = $faker->numberBetween(1 , 100);
+
+        $offrir->setMedicament($med[mt_rand(0,13)])
+               ->setRapportVisite($rapport_visites[mt_rand(0,30)])
+               ->setOffQte($qte);
     
+        $manager->persist($offrir);
+    }
+
+
+
+
 
         $manager->flush();
     }
