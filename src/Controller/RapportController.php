dddf77b50109474bb0875f68e6561f1edf0b93a8
sambouD <hungsas93@gmail.com>
2020-11-16 15:40:59 +0100

    ver5

--- src/Controller/RapportController.php
+++ src/Controller/RapportController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Praticien;
+use App\Entity\RapportVisite;
+use App\Form\RapportType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class RapportController extends AbstractController
 {
@@ -15,6 +22,24 @@ class RapportController extends AbstractController
     {
         return $this->render('/index.html.twig', [
             'controller_name' => 'RapportController',
+        ]);
+    }
+
+    /**
+     * Permet de créer le rapport de visite
+     * 
+     *@Route("/rapports/new", name="rapports_create")
+     
+     * @return Reponse
+     */
+    public function create(){
+        //Creation de form
+        $rapport_visite = new RapportVisite();
+        
+        $form = $this->createForm(RapportType::class, $rapport_visite);
+
+        return $this->render('rapport/new.html.twig',[
+            'form' => $form->createView()
         ]);
     }
 }
